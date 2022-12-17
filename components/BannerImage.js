c2bffa98b98c1a8fb93779583c036e7027d59a96
J S Vignesh Kanna <vkrishitaran@gmail.com>
2022-12-17 15:17:49 +0530

    monitor screeen size added

--- components/BannerImage.js
+++ components/BannerImage.js
@@ -4,7 +4,7 @@ import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 
 const BannerImage = () => {
   return (
-    <div className="pt-20 flex justify-between items-center">
+    <div className="pt-20 flex justify-between items-center pb-8">
       <div
         className=" hover:bg-gradient-to-r hover:from-black hover:to-[#0f171e38] my-auto h-[30vh] flex items-center pl-2 cursor-pointer
       "
@@ -14,6 +14,7 @@ const BannerImage = () => {
       <img
         src="https://m.media-amazon.com/images/S/sonata-images-prod/PV_IN_TVOD_DCDhamaka79/219bb6c2-4ca5-4d3e-8db7-f53cad44b232._UR3000,600_SX1500_FMwebp_.jpeg"
         alt="banner image"
+        className="max-w-7xl monitor:max-w-[1620px]"
       />
       <div className="hover:bg-gradient-to-l hover:from-black hover:to-[#0f171e38] my-auto h-[30vh] flex items-center pr-2 cursor-pointer">
         <FaChevronRight color="white" size={32} />
